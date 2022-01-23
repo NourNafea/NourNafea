a1847f2f8933e9770f404818e1a4692c01ce3fc8
NourNafea <nour68.nafea@gmail.com>
2022-01-23 21:58:14 +0200

    4th commit

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function generateProgressBar() {
         .join('')
     return `{ ${progressBar} }`
 }
+
+const readme = `\
+# Hi there! <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Hi.gif" width="29px">
+<p align="center">
+<a href="https://twitter.com/NoureldinNafea" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/twitter.svg" alt="NourNafea" height="30" width="30" /></a>&nbsp;
+<a href="https://www.linkedin.com/in/nour-nafea-572234173/" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg" alt="NourNafea" height="30" width="30" /></a>&nbsp;
+</p>
+
+![](https://camo.githubusercontent.com/992babdffd8c74a1502de375fbdf7e4d54773242/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f53576f536b4e36447854737a71494b4571762f67697068792e676966)
+
+`
+
+console.log(readme)
